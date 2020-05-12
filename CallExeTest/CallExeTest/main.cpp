c5d0a702bc964c868b8b11b71a8ab81c4b83233e
liguanlin <liguanlin@orbbec.com>
2020-05-12 17:35:29 +0800

    启动进程，并且等待

--- CallExeTest/CallExeTest/main.cpp
+++ CallExeTest/CallExeTest/main.cpp
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <shellapi.h>
+#include <iostream>
 
+using namespace std;
+
+#if 0
 int main()
 {
 	char ss[100];
 
 	//sprintf(ss, "%s -o %s %s", "E:\\VS2017WorkSpace\\SimpleRead\\x64\\SimpleRead.exe", "c:\\txt\\aaa.txt.swf", "\"c:\\txt\\a a a.txt\"");
-	snprintf(ss, 100, "%s", "DepthReaderPoll.exe");
+	//snprintf(ss, 100, "%s", "DepthReaderPoll.exe");
 
-	system(ss);
+	//system(ss);
 
-	//ShellExecute(NULL, "open", "calc.exe", "", "", SW_SHOW);
+	HINSTANCE hinstance = ShellExecute(NULL, "open", "calc.exe", "", "", SW_SHOW);
 
+	WaitForSingleObject(hinstance, 200000);
 	//ShellExecute(NULL, "open", "notepad.exe", "c:\\MyLog.log", "", SW_SHOW);
 
 	//ShellExecute(NULL, "open", "E:\\VS2017WorkSpace\\CallExeTest\\Debug\\DepthReaderPoll.exe", "", "", SW_SHOW);
@@ -24,6 +29,60 @@ int main()
 	
 	//相对路径，将DepthReaderPoll.exe 放在CallExeTest.exe所在目录
 	//ShellExecute(NULL, "open", "DepthReaderPoll.exe", "", "", SW_SHOW);
+
+	return 0;
+}
+
+#endif
+
+//使用createprocess的方式,可以等待
+int main()
+{
+
+	//SHELLEXECUTEINFO ShExecInfo = { 0 };
+	//ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+	//ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+	//ShExecInfo.hwnd = NULL;
+	//ShExecInfo.lpVerb = NULL;
+	//ShExecInfo.lpFile = "E:\\VS2017WorkSpace\\CallExeTest\\Debug\\DepthReaderPoll.exe";
+	//ShExecInfo.lpParameters = "";
+	//ShExecInfo.lpDirectory = NULL;
+	//ShExecInfo.nShow = SW_SHOW;
+	//ShExecInfo.hInstApp = NULL;
+	//ShellExecuteEx(&ShExecInfo);
+	//WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+	
+	PROCESS_INFORMATION ProcessInfo;
+	STARTUPINFO StartupInfo; //This is an [in] parameter
+	ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+	StartupInfo.cb = sizeof StartupInfo; //Only compulsory field
+	if (CreateProcess("E:\\VS2017WorkSpace\\CallExeTest\\Debug\\DepthReaderPoll.exe", NULL,
+		NULL, NULL, FALSE, 0, NULL,
+		NULL, &StartupInfo, &ProcessInfo))
+	{
+		WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+
+		DWORD dwExitCode;
+		if (GetExitCodeProcess(ProcessInfo.hProcess, &dwExitCode))
+		{
+			cout << dwExitCode << endl;
+			if (dwExitCode == 0)
+			{
+				cout << "sucess" << endl;
+			}
+			else
+			{
+				cout << "failed" << endl;
+			}
+		}
+
+		CloseHandle(ProcessInfo.hThread);
+		CloseHandle(ProcessInfo.hProcess);
+	}
+	else
+	{
+		printf("The process could not be started...");
+	}
 
 	return 0;
 }
